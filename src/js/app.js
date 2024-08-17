5d74079c77eb16d53b0a7e72b1f0b96806b905ff
Oleksii Petrychenko <aa.petrichenko@gmail.com>
2024-08-17 12:17:18 +0200

    Added buttons and counters in column headers

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import { Modal } from 'bootstrap'
 import { clock } from './helpers.js'
 
 setInterval(clock, 1000)
-clock
+
 
 // const instanceModal = new Modal('#exampleModal')
 // instanceModal.show()
